ea9bd376a37c744cf08d7b25a6341dc3d7270ecc
george <gibstep99@gmail.com>
2026-01-12 15:11:05 -0500

    _

--- firmware/Src/main.c
+++ firmware/Src/main.c
@@ -69,26 +69,15 @@ void SystemClock_Config(void);
   */
 int main(void)
 {
-
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
-
   /* Configure the system clock */
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
@@ -100,19 +89,13 @@ int main(void)
   MX_DAC1_Init();
   MX_TIM3_Init();
   MX_USART1_UART_Init();
+
   /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+  while (1) {
+    // UART loop
+    
   }
-  /* USER CODE END 3 */
+
 }
 
 /**
