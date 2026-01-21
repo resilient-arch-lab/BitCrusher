7bf47a86f2d0e0cc3aa73ec268afce3f183b9e3c
george <gibstep99@gmail.com>
2026-01-21 12:03:19 -0500

    not quite working...

--- resistor-network/ResistorNetwork/src/ResistorNetwork.jl
+++ resistor-network/ResistorNetwork/src/ResistorNetwork.jl
@@ -33,6 +33,55 @@ using ModelingToolkit: t_nounits as t
     return extend(sys, oneport)
 end
 
+@component function PowerResistor(R=1.0, P_max=0.5; name)
+    @named resistor = Resistor()
+    @unpack v, i = resistor
+
+    pars = @parameters begin
+        R = R
+        P_max = P_max
+    end
+
+    systems = @named begin
+        
+    end
+
+    vars = @variables begin
+        P(t), [description = "Power disipation"]
+    end
+
+    eqs = Equation[
+        P ~ v * i
+    ]
+
+    sys = System(eqs, t, vars, pars; name, systems)
+    return extend(sys, resistor)
+end
+
+function PowerResistorTB() 
+    systems = @named begin
+        resistor = PowerResistor()
+        ground = Ground()
+        source = Voltage()
+        magnitude = Constant(k = 1.0)
+    end
+
+    eqs = Equation[
+        source.V ~ magnitude.output,
+        source.n ~ ground.g ~ resistor.n,
+        source.p ~ resistor.p
+    ]
+
+    @named sys = System(eqs, t, [], []; systems=systems)
+    model = mtkcompile(sys)
+    prob = ODEProblem(model, Pair[], (0, 10))
+    sol = solve(prob)
+
+    plot(sol, idxs = [model.resistor.v, model.resistor.P],
+    title = "RC Circuit Demonstration",
+    labels = ["Resistor voltage" "Resistor power dissipation"])
+end
+
 @named test_network = SeriesNetwork(10)
 model = mtkcompile(test_network)
 prob = ODEProblem(model, Pair[], (0, 10))
