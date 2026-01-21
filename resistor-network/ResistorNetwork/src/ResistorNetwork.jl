932b3a9161bb78de5e89c86f06662d6e918bba60
George <gibstep99@gmail.com>
2026-01-21 16:25:13 -0500

    working on resistor network equivalence calculations

--- resistor-network/ResistorNetwork/src/ResistorNetwork.jl
+++ resistor-network/ResistorNetwork/src/ResistorNetwork.jl
@@ -5,7 +5,7 @@ using Plots
 using OrdinaryDiffEq
 
 using ModelingToolkitStandardLibrary.Electrical
-using ModelingToolkitStandardLibrary.Blocks: Constant
+using ModelingToolkitStandardLibrary.Blocks: Constant, Step, Sine
 using ModelingToolkit: t_nounits as t
 
 @component function SeriesNetwork(ESR = 1.0; name)
@@ -58,50 +58,54 @@ end
     return extend(sys, resistor)
 end
 
-@component function PowerResistorTB(; name)
-    pars = @parameters begin
-    end
-
+function PowerResistorTB()
     systems = @named begin
-        resistor = PowerResistor()
-        ground = Ground()
+        magnitude = Sine(offset = 1, amplitude = 10, frequency = 5)
         source = Voltage()
-        magnitude = Constant(k = 1.0)
+        resistor = PowerResistor()
+        capacitor = Capacitor(C = 1, v = 0.0)
+        ground = Ground()
     end
 
-    # vars = @variables begin
-    #     P(t), [description = "Resistor Power Disipation"]
-    #     v(t), [description = "Resistor Voltage Drop"]
-    # end
-
-    eqs = Equation[
-        source.V ~ magnitude.output,
-        source.n ~ ground.g ~ resistor.n,
-        source.p ~ resistor.p
+    eqs = [
+        connect(magnitude.output, source.V),
+        connect(resistor.n, source.n, capacitor.n, ground.g),
+        connect(resistor.p, source.p, capacitor.p),
     ]
 
-    sys = System(eqs, t; name=name, systems=systems)
+    @named sys = System(eqs, t; systems=systems)
     return sys
 end
 
+# Works now!
 function test_power_resistor()
-    @named sys = PowerResistorTB()
+    sys = PowerResistorTB()
     model = mtkcompile(sys)
     prob = ODEProblem(model, Pair[], (0, 10))
     sol = solve(prob)
 
-    plot(sol, idxs = [model.v, model.P],
-        title = "RC Circuit Demonstration",
+    plot(sol, idxs = [model.resistor.v, model.resistor.P],
+        title = "Power Resistor",
         labels = ["Resistor voltage" "Resistor power dissipation"])
 end
 
-@named test_network = SeriesNetwork(10)
-model = mtkcompile(test_network)
-prob = ODEProblem(model, Pair[], (0, 10))
-sol = solve(prob)
-plot(sol, idxs = [model.v, model.r1.v, model.r2.v],
-    title = "RC Circuit Demonstration",
-    labels = ["Network Voltage" "R1 Voltage" "R2 Voltage"])
+function find_resistor_network(resistor::System, R_V_max::Real, R_P_max::Real)
+    ESR = resistor.R
+    P = resistor.P_max
 
+    # Calculate network parallel size
+    n_parallel = cld(P, R_P_max)
+    # Calculate ESR for each branch
+    parallel_ESR = ESR^(1/n_parallel)
+
+    # Now each branch must be:
+    #   - Identical
+    #   - Each resistor dissipating less than R_P_max
+    #   - Each resistor voltage drop less than R_V_max
+    #   - Made of only E24 values
+
+    
+
+end
 
 end # module ResistorNetwork
