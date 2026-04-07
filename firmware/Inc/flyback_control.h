f9727893017148271c2868cf74f857eb3bcb3bd6
George <gibstep99@gmail.com>
2026-04-07 13:41:33 -0400

    control system tuning

--- firmware/Inc/flyback_control.h
+++ firmware/Inc/flyback_control.h
@@ -9,13 +9,13 @@ extern "C" {
 
 // voltage feedback conversion macros
 // #define HVVS_MAX 3
-#define HVVS_MAX 2.7
+#define HVVS_MAX 1.5
 #define HVVS_MIN 0
 #define VDDA (float )3.3
 #define HV_MIN 150
 #define HV_MAX 500
 #define HV_COMP_WINDOW 0.1
-#define RAMP(setpoint) 0.1 * (setpoint / HV_MAX)
+#define RAMP(setpoint) 0 * (setpoint / HV_MAX)
 
 // PWM duty cycle macros
 #define D_MIN 0.08
@@ -29,7 +29,7 @@ extern "C" {
 #define D_TO_ILIM(D) ((V_ILIM_MIN - V_ILIM_MAX)*D) + V_ILIM_MIN
 
 #define ADC_TO_V(x) (VDDA/(0x0fff))*x
-#define VHV_OFFSET 0.75
+#define VHV_OFFSET 0.85
 #define V_TO_VHV(x) ((float )HV_MAX/(float )(HVVS_MAX - HVVS_MIN))*(x - VHV_OFFSET)
 
 typedef struct {
