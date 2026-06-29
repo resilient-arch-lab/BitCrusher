cc3a8566457f9c87c61007bffa301d018f5edd3c
george crane <gibstep99@gmail.com>
2026-06-29 16:53:56 -0400

    _

--- design-resources/flyback-modeling/flyback.jl
+++ design-resources/flyback-modeling/flyback.jl
@@ -7,14 +7,13 @@ using ControlSystemsBase
 
 
 
-function FlybackCCMAveraged( ; name, V_d=1.7, R=1.5e6, R_c=0.05, R_sw=2, C=5e-6, L_m=10e-6, N=0.1) 
+function FlybackCCMAveraged( ; name, V_d=1.7, R=1.5e6, R_c=0.05, R_sw=2, C_L=5e-6, L_m=10e-6, N=0.1, D1=0.2) 
     @named vp_primary = Pin()
     @named vm_primary = Pin()
     @named vp_secondary = Pin()
     @named vm_secondary = Pin()
-    @named din = RealInput()
     
-    @parameters R_c = R_c R = R R_sw = R_sw C = C L_m = L_m V_d = V_d N = N
+    @parameters R_c = R_c R = R R_sw = R_sw C_L = C_L L_m = L_m V_d = V_d N = N D1=D1
 
     @variables begin
         # state vars 
@@ -23,23 +22,21 @@ function FlybackCCMAveraged( ; name, V_d=1.7, R=1.5e6, R_c=0.05, R_sw=2, C=5e-6,
         
         # input vars  ( can the V_d parameter be assigned as a variable)
         v_s(t) 
-        # Vd(t) = V_d  
+        # Vd(t) = V_d
         
         v(t)  # output vars 
         
         # d(t)  # interval 1 duty cycle
     end
 
-    @unpack u = din
-    d = u
 
     A1 = [
         -R_sw/L_m       0; 
-        0               -1/((R*C)+R_c)
+        0               -1/((R*C_L)+R_c)
     ]  # interval 1 state matrix
     A2 = [
-        (N^2 * R_c * R)/(R*L_m - R_c*C) (N*R)/(R*L_m - R_c*L_m);
-        -(N*R)/(R*C - R_c*C)            -(1)/(R*C + R_c*C)
+        (N^2 * R_c * R)/(R*L_m - R_c*C_L) (N*R)/(R*L_m - R_c*L_m);
+        -(N*R)/(R*C_L - R_c*C_L)            -(1)/(R*C_L + R_c*C_L)
     ]  # interval 2 state matrix
 
     B1 = [
@@ -62,13 +59,17 @@ function FlybackCCMAveraged( ; name, V_d=1.7, R=1.5e6, R_c=0.05, R_sw=2, C=5e-6,
     y = [i_Lm; v]
 
     eqs = [
-        D.(x) ~ ((A1.*d + A2.*(1-d)) * x) + ((B1.*d + B2.*(1-d)) * [v_s; V_d])
-        y ~ ((C1.*d + C2.*(1-d)) * x)
-        y ~ [vp_secondary.v - vm_secondary.v]
+        D.(x) ~ (A * x) + (B * [v_s; V_d])
+        y ~ (C * x)
+        vp_primary.i + vm_primary.i ~ 0
+        vp_secondary.i + vm_secondary.i ~ 0
+        v ~ vp_secondary.v - vm_secondary.v
         v_s ~ vp_primary.v - vm_primary.v
+        # vm_secondary.v ~ vm_primary.v
+        i_Lm ~ vp_primary.i
     ]
 
-    System(eqs, t, [i_Lm, v_c, v_s, Vd, v, d], [R_c, R, R_sw, C, L_m, V_d, N]; systems = [vp_primary, vm_primary, vp_secondary, vm_secondary], name)
+    System(eqs, t, [i_Lm, v_c, v_s, v], [R_c, R, R_sw, C_L, L_m, V_d, N, D1]; systems = [vp_primary, vm_primary, vp_secondary, vm_secondary], name)
 end
 
 # inputs: primary curret, capacitor voltage
@@ -150,3 +151,8 @@ eqs = [
     mtkss.input.u[1] ~ 1  # 1A
     mtkss.input.u[2] ~ mtkss.output.u[2]
 ]
+@named mtkmodel = System(eqs, t, [], []; systems=[mtkss])
+compiled= mtkcompile(mtkmodel)
+prob = ODEProblem(compiled, [], (0.0, 0.005))
+sol = solve(prob)
+plot(sol; dpi=300)
