a4321f085ecbf6823e8ad462c92f37e4441250e4
george crane <gibstep99@gmail.com>
2026-07-12 20:15:22 -0400

    _

--- design-resources/flyback-modeling/flyback-real.jl
+++ design-resources/flyback-modeling/flyback-real.jl
@@ -80,6 +80,27 @@ function IdealTransformer(; name, Nps)
     System(eqs, t, [v1, v2, i1, i2], [Nps], systems=[p1, p2, n1, n2], name=name)
 end
 
+function ControlledSwitch(; name)
+    @named n = Pin()
+    @named p = Pin()
+    @named u = RealInput()
+
+    @variables begin
+        v(t)
+        i(t)
+    end
+
+    eqs = [ 
+        0 ~ ifelse(u.u > 0.5, n.v - p.v, n.i)
+        0 ~ ifelse(u.u > 0.5, n.i + p.i, p.i)
+        v ~ p.v - n.v
+        i ~ p.i
+        # p.i + n.i ~ 0
+        # 0 ~ ifelse(u.u > 0.5, v, i)
+    ]
+    ODESystem(eqs, t, [v, i], []; name, systems=[p, n, u])
+end
+
 
 function FlybackTransformer(; name, L_m=10e-6, Nps=0.1)
     @parameters begin
@@ -102,6 +123,7 @@ function FlybackTransformer(; name, L_m=10e-6, Nps=0.1)
         iout(t)  # output port current
         ilm(t)  # magnetizing inductance current
         itp(t)  # transformer primary current
+        iloop(t)
     end
 
     eqs = [
@@ -118,6 +140,7 @@ function FlybackTransformer(; name, L_m=10e-6, Nps=0.1)
         # Primary side current properties
         itp ~ transformer_port.i
         ilm ~ magnetizing_inductor.i
+        iloop ~ magnetizing_inductor.p.i - transformer_port.p.i
         # iin ~ itp + ilm
 
         # Ideal transformer equations
@@ -128,7 +151,7 @@ function FlybackTransformer(; name, L_m=10e-6, Nps=0.1)
         connect(n1, magnetizing_inductor.n, transformer_port.n)
     ]
 
-    System(eqs, t, [vin, vout, iin, iout, ilm, itp], [Nps, L_m], systems=[p1, p2, n1, n2, magnetizing_inductor, transformer_port], name=name)
+    System(eqs, t, [vin, vout, iin, iout, ilm, itp, iloop], [Nps, L_m], systems=[p1, p2, n1, n2, magnetizing_inductor, transformer_port], name=name)
 end
 
 function PerfectDiode(; name)
@@ -201,10 +224,8 @@ function CoupledInductorTest2(; name)
     @named resistor_load = Resistor(R=1.5e6)
 
     # @named diode = Diode(Is=1e-6, n=0.95)
-    @named diode = VariableResistor(R_ref = 1e7)
-    # @named diode_r_s = Resistor(R=1)    # no impact on stability
-    # @named diode_r_p = Resistor(R=1e6)  # no impact on stability
-
+    # @named diode = VariableResistor(R_ref = 1e7)
+    @named diode = ControlledSwitch()
 
     test_system_eqs = [
         source_val.u ~ ifelse(
@@ -215,15 +236,11 @@ function CoupledInductorTest2(; name)
         connect(source_val, source.I)
         connect(source.p, transformer.p1)
 
-        # connect(transformer.p2, diode_r_s.p, diode_r_p.p)
-        # connect(diode_r_p.n, diode.n)
-
-        # connect(diode_r_s.n, diode.p)
-        # connect(diode.n, capacitor_load.p, resistor_load.p)
+        # diode.u.u ~ ifelse(transformer.p2.v - capacitor_load.p.v < 0, 0, 1)
+        diode.u.u ~ ifelse(diode.v < 0, 1, 1)
         # connect(transformer.p2, capacitor_load.p, resistor_load.p)
         connect(transformer.p2, diode.p)
         connect(diode.n, capacitor_load.p, resistor_load.p)
-        diode.position.u ~ ifelse(diode.v <= 0, 1, 0)
         
         # connect(transformer.n1, resistor_p.p)
         # connect(source.n, resistor_p.n, transformer.n2, capacitor_load.n, resistor_load.n, gnd.g)
@@ -233,13 +250,63 @@ function CoupledInductorTest2(; name)
     System(test_system_eqs, t, [], [f, Imax, Tstop, start_time], systems=[transformer, gnd, source, source_val, capacitor_load, resistor_load, diode], initial_conditions=[transformer.vout => 0]; name=name)
 end
 
-@named test_system = CoupledInductorTest2()
+function CoupledInductorTest3(; name)
+    @parameters begin
+        f = 100000
+        Imax = 1
+        Tstop = 0.00006
+        start_time=0
+    end
+
+    @named transformer = FlybackTransformer(L_m=10e-6, Nps=0.1)
+    @named gnd = Ground()
+    @named source = Voltage()
+    @named source_val = RealOutput()
+    @named capacitor_load = Capacitor(C=5e-6, v=0.0)
+    # @named resistor_p = Resistor(R=0.1)
+    @named resistor_load = Resistor(R=1.5e6)
+
+    # @named diode = Diode(Is=1e-6, n=0.95)
+    # @named diode = VariableResistor(R_ref = 1e7)
+    @named diode = ControlledSwitch()
+    @named switch = ControlledSwitch()
+
+    test_system_eqs = [
+        # source_val.u ~ ifelse(
+            # ((4 * floor(f * (t - start_time)) - 2 * floor(2 * (t - start_time) * f) + 2) >= 1), 
+            # (Imax * 2) * ((-(t)*1e5) - (-floor(f * (t - start_time)))) * (t<=Tstop),
+            # 0
+        # )
+        source_val.u ~ 5
+        connect(source_val, source.V)
+        connect(source.p, transformer.p1)
+
+        connect(switch.p, transformer.n1)
+        switch.u.u ~ sin((8e4)*2*pi*t)
+
+        # diode.u.u ~ ifelse(transformer.p2.v - capacitor_load.p.v < 0, 0, 1)
+        diode.u.u ~ ifelse(diode.v < 0, 1, 1)
+        # connect(transformer.p2, capacitor_load.p, resistor_load.p)
+        connect(transformer.p2, diode.p)
+        connect(diode.n, capacitor_load.p, resistor_load.p)
+        
+        # connect(transformer.n1, resistor_p.p)
+        # connect(source.n, resistor_p.n, transformer.n2, capacitor_load.n, resistor_load.n, gnd.g)
+        # connect(source.n, transformer.n1, transformer.n2, capacitor_load.n, resistor_load.n, gnd.g)
+        connect(source.n, switch.n, transformer.n2, capacitor_load.n, resistor_load.n, gnd.g)
+    ]
+
+    System(test_system_eqs, t, [], [f, Imax, Tstop, start_time], systems=[transformer, gnd, source, source_val, capacitor_load, resistor_load, diode, switch], initial_conditions=[transformer.vout => 0]; name=name)
+end
+
+
+@named test_system = CoupledInductorTest3()
 test_system_compiled = mtkcompile(test_system)
 prob = ODEProblem(test_system_compiled, [], (0.0, 2e-4))
 sol = solve(prob, Rodas5P(), abstol=1e-9, reltol=1e-12)
 plot(
     sol, 
-    idxs=[test_system_compiled.transformer.iin, test_system_compiled.transformer.itp, test_system_compiled.transformer.ilm, test_system_compiled.transformer.iout],
+    idxs=[test_system_compiled.transformer.iin, test_system_compiled.transformer.itp, test_system_compiled.transformer.ilm, test_system_compiled.transformer.iout, test_system_compiled.diode.v, test_system_compiled.transformer.vin, test_system_compiled.transformer.iloop],
     # idxs=[test_system_compiled.L1.p1.i, test_system_compiled.L1.p2.i, test_system_compiled.L1.p2.v, test_system_compiled.C1.v],
     dpi=300
 )
